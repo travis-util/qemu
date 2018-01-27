daac717950288d9a65852f5159653be11356610b
Nardberjean <Nardberjean@users.noreply.github.com>
2018-01-27 15:10:12 -0500

    Create finnix.py

---
+++ finnix.py
@@ -0,0 +1,12 @@
+import pexpect, sys
+child = pexpect.spawn('qemu-system-x86_64 -cdrom finnix-111.iso')
+child.logfile = sys.stdout
+# child.expect('Please press Enter to activate this console.')
+# child.sendline('')
+# child.expect('root@(none):/#')
+# child.sendline('uname -a')
+# child.sendline('dpkg')
+# child.sendline('dpkg update')
+# child.sendline('halt')
+# child.expect(pexpect.EOF)
+print child.before;
