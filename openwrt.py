e195784cbc736e309ba3390dc9c0eba02f009fb6
Nardberjean <Nardberjean@users.noreply.github.com>
2018-01-27 14:09:10 -0500

    Update openwrt.py

--- openwrt.py
+++ openwrt.py
@@ -1,4 +1,4 @@
 import pexpect
 child = pexpect.spawn('qemu-system-arm -M realview-eb-mpcore -kernel openwrt-realview-vmlinux-initramfs.elf -nographic')
-child.expect(Please press Enter to activate this console.')
+child.expect('Please press Enter to activate this console.')
 child.sendline('')
