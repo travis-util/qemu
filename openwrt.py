fc837c122645a9615ebcc257aaf09cdbccdfa5b4
Nardberjean <Nardberjean@users.noreply.github.com>
2018-09-22 13:47:55 -0400

    Update openwrt.py

--- openwrt.py
+++ openwrt.py
@@ -1,6 +1,7 @@
 import pexpect, sys
 # child = pexpect.spawn('qemu-system-arm -M realview-eb-mpcore -kernel openwrt-realview-vmlinux-initramfs.elf -nographic') # Obsolete
 child = pexpect.spawn('qemu-system-arm -net nic,vlan=0 -net nic,vlan=1 -net user,vlan=1 -nographic -M virt -m 64 -kernel openwrt-armvirt-32-zImage-initramfs')
+child = pexpect.spawn('qemu-system-arm -nographic -M virt -m 64 -kernel openwrt-armvirt-32-zImage-initramfs')
 child.logfile = sys.stdout
 child.expect('Please press Enter to activate this console.')
 child.sendline('')
