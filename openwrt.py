c04f16369947ea346ec3a812d0e56f471982d805
Nardberjean <Nardberjean@users.noreply.github.com>
2018-09-22 11:17:55 -0400

    Update openwrt.py

--- openwrt.py
+++ openwrt.py
@@ -1,6 +1,5 @@
 import pexpect, sys
-# child = pexpect.spawn('qemu-system-arm -M realview-eb-mpcore -kernel openwrt-realview-vmlinux-initramfs.elf -nographic')
-# child = pexpect.spawn('qemu-system-arm -net nic,vlan=0 -net nic,vlan=1 -net user,vlan=1 -nographic -M virt -m 64 -kernel openwrt-armvirt-64-Image-initramfs')
+# child = pexpect.spawn('qemu-system-arm -M realview-eb-mpcore -kernel openwrt-realview-vmlinux-initramfs.elf -nographic') # Obsolete
 child = pexpect.spawn('qemu-system-arm -net nic,vlan=0 -net nic,vlan=1 -net user,vlan=1 -nographic -M virt -m 64 -kernel openwrt-armvirt-32-zImage-initramfs')
 child.logfile = sys.stdout
 child.expect('Please press Enter to activate this console.')
@@ -13,7 +12,7 @@ child.sendline('cat /proc/cmdline')
 child.expect('root')
 child.sendline('opkg')
 child.expect('root')
-# child.sendline('opkg update') # Internet connection not working! should work now...
+child.sendline('opkg update') # Internet connection not working! should work now...
 # child.sendline('halt')
 # child.expect(pexpect.EOF)
 print child.before;
